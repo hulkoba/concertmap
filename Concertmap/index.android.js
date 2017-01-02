2f851796542ced3706277b67da546a738f9b9246
Jacoba Brandner <jacoba.brandner@web.de>
2017-01-02 14:01:51 +0100

    style(list): add img and style complete

--- Concertmap/index.android.js
+++ Concertmap/index.android.js
@@ -9,6 +9,7 @@ import {
   AppRegistry,
   ListView,
   Text,
+  Image,
   View,
   ActivityIndicator
 } from 'react-native';
@@ -65,26 +66,37 @@ class Concertmap extends Component {
     });
   }
 
-  // render functions
+  // source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+  
   renderRow = (movies) => {
     return (
       <View style={listStyles.row}>
-        <View style={listStyles.titleRow}>
-        <Text style={listStyles.title}>
-          {movies.title}  
-          <Text style={listStyles.distance}>
-            {'900'}{'m'}
-          </Text>        
-        </Text>
+        <View style={listStyles.imageView}>
+          <Image style={listStyles.image}
+            source={require('./styles/pugtato.png')} />
         </View>
-     
-        <Text style={listStyles.subTitle}>
-          {movies.releaseYear}{'\n'}
-        </Text>
-        <Text style={listStyles.description} numberOfLines={3}>
-          {'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius'}
-        </Text>
+
+        <View style={listStyles.column}>
+          <View style={listStyles.titleRow} numberOfLines={1}>
+            <Text style={listStyles.title}>
+              {movies.title}              
+            </Text>
+            <Text style={listStyles.distance}>
+              {'900'}{'m'}
+            </Text>        
+          </View>
+          
+          
         
+          <Text style={listStyles.subTitle} numberOfLines={1}>
+            {movies.releaseYear}
+          </Text>      
+        
+          <Text style={listStyles.description} numberOfLines={3}>
+            {'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. '}
+          </Text>
+        </View>
+       
       </View>
     )
   }
--- Concertmap/styles/styles.js
+++ Concertmap/styles/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#1a1a1a',
   },
   center: {
     flex: 1,
@@ -15,27 +15,42 @@ export const styles = StyleSheet.create({
 export const listStyles = StyleSheet.create({
   row: {    
     padding: 14,
+    paddingTop: 0,
+    backgroundColor: '#1a1a1a',
+    flexDirection: 'row',
+  },
+  column: {    
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: 40,
     borderBottomWidth: 1,
     borderBottomColor: '#363636',
-    backgroundColor: '#1a1a1a',
+    paddingVertical: 15,
+    paddingRight: 15,
   },
   image: {
-
+    width: 75,
+    height: 75,
+  },
+  imageView: {
+    width: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   titleRow: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',    
+    flex: 1
   },
-  title: {
+  title: {    
     fontFamily: 'Roboto',
     fontSize: 22,
-    color: '#e6e6e6',
-    flex: 1,
-    
+    color: '#e6e6e6',    
   },
   distance: {
     fontSize: 14,
-    color: '#363636',    
-    flex: 1,
+    color: '#363636',
   },
   subTitle: {
     fontFamily: 'Roboto',
@@ -45,6 +60,6 @@ export const listStyles = StyleSheet.create({
   description: {
     fontFamily: 'Roboto',
     fontSize: 10,
-    color: '#363636'
+    color: '#363636',
   },
 });
